5d1ddeaf5ca458bd7b6141e1711689f80d7088f5
CodeLuminary <victorijoni@yahoo.com>
2021-12-25 02:47:41 +0100

    Island count added

--- Graph algorithm/Indrectional Graph/Challenges/graph.js
+++ Graph algorithm/Indrectional Graph/Challenges/graph.js
@@ -96,4 +96,39 @@ const buildGraph = (edges) =>{
     }
 }
 
-shortestPossiblePath(edges,'w','z')
+shortestPossiblePath(edges,'w','z');
+
+const islandCount = (grid) =>{
+    const visited = new Set();
+    let count = 0; 
+
+    for(let r = 0; r < grid.length; r+=1){
+        for(let c = 0; c < grid[0].length; c +=1){
+            if(exploreIslandCount(grid,r,c,visited)===true){
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+const exploreIslandCount = (grid, r, c, visited)=>{
+    const rowInbounds = 0 <= r && r < grid.length;
+    const colInbounds = 0 <= c && c < grid[0].length;
+
+    if(!rowInbounds || !colInbounds) return false;
+
+    if(grid[r][c] === 'w') return false;
+
+    const pos = r + ',' + c;
+    if(visited.has(pos)) return false;
+    visited.add(pos);
+
+    exploreIslandCount(grid, r-1, c, visited);
+    exploreIslandCount(grid, r+1, c, visited);
+    exploreIslandCount(grid, r, c-1, visited);
+    exploreIslandCount(grid, r, c+1, visited);
+
+    return true;
+}
