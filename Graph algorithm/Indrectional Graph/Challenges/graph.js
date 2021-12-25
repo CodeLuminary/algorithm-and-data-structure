3d29cdef11239eb67d650a427a93e8c8dcc61d1e
CodeLuminary <victorijoni@yahoo.com>
2021-12-25 02:20:46 +0100

    shortest possible path challenge added

--- Graph algorithm/Indrectional Graph/Challenges/graph.js
+++ Graph algorithm/Indrectional Graph/Challenges/graph.js
@@ -1,3 +1,4 @@
+//Solves the connected components counts
 const connectedComponentCount = (graph) =>{
     const visited = new Set();
     for(let node in graph){
@@ -17,6 +18,30 @@ const explore = (graph, currrent, visited) =>{
     return true;
 }
 
+const largestComponent = (graph)=>{
+    const visited = new Set();
+    let longest = 0;
+    for(let node in graph){
+        const size = exploreSize(graph,node,visited);
+        if(size > longest) longest = size;
+    }
+    return longest;
+}
+
+const exploreSize = (graph, node, visited)=>{
+    if(visited.has(node)) return 0;
+
+    visited.add(node);
+
+    let size = 1;
+
+    for(let neighbor of graph[node]){
+        size += exploreSize(graph, neighbor, visited);
+    }
+
+    return size;
+}
+
 connectedComponentCount({
     0:[8,1,5],
     1:[0],
@@ -26,3 +51,49 @@ connectedComponentCount({
     3:[2,4],
     4:[3,2]
 })
+
+const edges = [
+    ['w', 'x'],
+    ['x', 'y'],
+    ['z', 'y'],
+    ['z', 'y'],
+    ['w', 'v']
+];
+
+const shortestPossiblePath = (edges,nodeA,nodeB)=>{
+    const graph = buildGraph(edges);
+    console.log(graph);
+
+    const visited = new Set([ nodeA ]);
+    const queue = [ [nodeA, 0]];
+
+    while(queue.length > 0){
+        const [node, distance] = queue.shift();
+
+        if(node === nodeB) return distance;
+
+        for(let neighbor of graph[node]){
+            if(!visited.has(neighbor)){
+                visited.add(neighbor);
+                queue.push([ neighbor, distance + 1]);
+            }
+        }
+    }
+
+    return -1;
+}
+
+const buildGraph = (edges) =>{
+    const graph = {};
+
+    for(let edge in edges){
+        const [a, b] = edge;
+        if(!(a in graph)) graph[a] = []
+        if(!(a in graph)) graph[b] = []
+
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+}
+
+shortestPossiblePath(edges,'w','z')
